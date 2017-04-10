a8d199b306f06ffc51f3cc74db9104692be02059
Oggi Danailov <ogidan@abv.bg>
2017-04-10 18:15:26 -0400

    general layout of page1

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-window.onload = function(){
+// window.onload = function(){
 
-	setTimeout(function(){ document.write('L') }, 1000);	
-	setTimeout(function(){ document.write('i') }, 1200);
-	setTimeout(function(){ document.write('e') }, 1500);
-	setTimeout(function(){ document.write('u') }, 1900);
-	setTimeout(function(){ document.write('t') }, 2100);
-	setTimeout(function(){ document.write('e') }, 2330);
-	setTimeout(function(){ document.write('n') }, 2450);
-	setTimeout(function(){ document.write('a') }, 2850);
-	setTimeout(function(){ document.write('n') }, 3050);
-	setTimeout(function(){ document.write('t') }, 3180);
-	setTimeout(function(){ document.write(' ') }, 3480);
+// 	setTimeout(function(){ document.write('L') }, 1000);	
+// 	setTimeout(function(){ document.write('i') }, 1200);
+// 	setTimeout(function(){ document.write('e') }, 1500);
+// 	setTimeout(function(){ document.write('u') }, 1900);
+// 	setTimeout(function(){ document.write('t') }, 2100);
+// 	setTimeout(function(){ document.write('e') }, 2330);
+// 	setTimeout(function(){ document.write('n') }, 2450);
+// 	setTimeout(function(){ document.write('a') }, 2850);
+// 	setTimeout(function(){ document.write('n') }, 3050);
+// 	setTimeout(function(){ document.write('t') }, 3180);
+// 	setTimeout(function(){ document.write(' ') }, 3480);
 
-	setTimeout(function(){ document.write('S') }, 3780);
-	setTimeout(function(){ document.write('c') }, 3980);
-	setTimeout(function(){ document.write('h') }, 4130);
-	setTimeout(function(){ document.write('m') }, 4430);
-	setTimeout(function(){ document.write('i') }, 4530);
-	setTimeout(function(){ document.write('d') }, 4730);
-	setTimeout(function(){ document.write('t') }, 4930);
-}
+// 	setTimeout(function(){ document.write('S') }, 3780);
+// 	setTimeout(function(){ document.write('c') }, 3980);
+// 	setTimeout(function(){ document.write('h') }, 4130);
+// 	setTimeout(function(){ document.write('m') }, 4430);
+// 	setTimeout(function(){ document.write('i') }, 4530);
+// 	setTimeout(function(){ document.write('d') }, 4730);
+// 	setTimeout(function(){ document.write('t') }, 4930);
+// }
 
 
