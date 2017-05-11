b2b2d57b299ebc6661c35e38231b81bf434db8f2
Oggi Danailov <ogidan@abv.bg>
2017-05-10 22:45:03 -0400

    audio playhead representation fixed

--- js/music.js
+++ js/music.js
@@ -1,13 +1,14 @@
 var audio = new Audio;
 
-// var play1 = document.getElementById('play1')
-// var pause1 = document.getElementById('pause1')
-// var next1 = document.getElementById('next1')
+var play1 = document.getElementById('play1')
+var pause1 = document.getElementById('pause1')
+var next1 = document.getElementById('next1')
 var songList = document.getElementById('songList')
 
 var jukebox = new Jukebox()
 
 play1.addEventListener('click', jukebox.play)
+play1.addEventListener('click', jukebox.timeLineRepresentation)
 pause1.addEventListener('click', jukebox.pause)
 next1.addEventListener('click', jukebox.next)
 previous1.addEventListener('click', jukebox.previous)
@@ -27,13 +28,6 @@ var songs = [jorge, run, ukulele, extension, blues, arabesque, eye, siberia, sno
 
 
 
-
-
-// for(var i=0;i<songs.length;i++){
-// 	$('#songList').append('<li><a href="audio/">' + songs[i].name +".mp3"+ '</a></li>')
-// }
-
-
 $(document).ready(function() {
   for (i=0;i<songs.length;i++) {
     var song = document.createElement("h3");
@@ -45,7 +39,7 @@ $(document).ready(function() {
 
   $.each(songNames, function(index, value) {
     songStrings.push(value.innerText)
-    console.log(songStrings)
+    // console.log(songStrings)
   });
 
   $("h3").click(function() {
@@ -69,14 +63,15 @@ function Jukebox(){
 	this.next = next;
 	this.previous = previous;
 	this.random = random;
+	this.timeLineRepresentation = timeLineRepresentation;
 }
 
 
 function play(){
-	audio.src = songs[currentSong].fileName;
+	audio.src = songs[currentSong].fileName;	
 	
-	
-	audio.play()
+		audio.play()
+
 	setTimeout(function(){
 		audio.duration
 	}, 100)
@@ -85,25 +80,29 @@ function play(){
 function stop() {
 	audio.src = songs[currentSong].fileName;
 	audio.pause()
-	console.log(currentSong)
+	// console.log(currentSong)
 }
+
+
+
+
 function next(){
 	 	if(currentSong == songs.length - 1){
 	 		currentSong = 0;
 	 		audio.src = songs[currentSong].fileName;
 	 		audio.play()
-	 		console.log(currentSong)
+	 		// console.log(currentSong)
 	 	}else{
 	 		currentSong = currentSong +  1;
 	 		audio.src = songs[currentSong].fileName;
 	 		audio.play()
-	 		console.log(currentSong)
+	 		// console.log(currentSong)
 	 	}
 }
 
 function previous(){
 	 	currentSong = currentSong -  1;
-	 	console.log(currentSong)
+	 	// console.log(currentSong)
 	 	if(currentSong < 0){
 	 		currentSong = songs.length-1;
 	 		audio.src = songs[currentSong].fileName;
@@ -118,6 +117,56 @@ function random(x, y){
 	currentSong = Math.floor(Math.random()* (songs.length-1 * 0)) + 0;
 	audio.src = songs[currentSong].fileName;
 	audio.play()
-	console.log(currentSong)
+	// console.log(currentSong)
+}
+
+function timeLineRepresentation(){
+		var totalLength = timeline.offsetWidth - playhead.offsetWidth
+// console.log(totalLength)
+		audio.addEventListener('timeupdate', timeUpdate)
+
+		audio.addEventListener("canplaythrough", function() {
+		    duration = audio.duration;
+		}, false);
+
+		timeline.addEventListener('click', function(event){
+			moveplayhead(event)
+		   audio.currentTime = duration * clickPercent(event);
+		}, false);
+
+		// returns click as decimal (.77) of the total timelineWidth
+		function clickPercent(event) {
+		    return (event.clientX - getPosition(timeline)) / totalLength;
+
+		}
+
+		function moveplayhead(event) {
+		    var newMargLeft = event.clientX - getPosition(timeline);
+
+		    if (newMargLeft >= 0 && newMargLeft <= totalLength) {
+		        playhead.style.marginLeft = newMargLeft + "px";
+		    }
+		    if (newMargLeft < 0) {
+		        playhead.style.marginLeft = "0px";
+		    }
+		    if (newMargLeft > totalLength) {
+		        playhead.style.marginLeft = totalLength + "px";
+		    }
+		}
+
+		function timeUpdate() {
+		    var playPercent = totalLength * (audio.currentTime / duration);
+		    playhead.style.marginLeft = playPercent + "px";
+		    if (audio.currentTime == duration) {
+		        pButton.className = "";
+		        pButton.className = "play";
+		    }
+		}
+
+
+
+		function getPosition(el) {
+		    return el.getBoundingClientRect().left;
+		}
 }
 
